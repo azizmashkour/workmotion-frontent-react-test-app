ca78355e3b04bc5b46dd260d0d9812c8036a7d7b
mashorbit <mashkour@orbit.love>
2022-04-08 20:44:33 +0200

    [employees-management]: create app http request config and constants

---
+++ src/constants/Configs.ts
@@ -0,0 +1,11 @@
+// Used for Employee state management in `States/States.tsx`
+export const Statuses = ['added', 'in-check', 'approved', 'active', 'inactive']
+
+// Used for the Employee Role select dropdown in the `Form/AddEmployee.tsx` form.
+export const Options = [
+  {'id': 1,'name': 'Choose...'},
+  {'id': 2,'name': 'Secretary'},
+  {'id': 3,'name': 'Accounting'},
+  {'id': 4,'name': 'Senior Developer'},
+  {'id': 5,'name': 'CTO'}
+]
